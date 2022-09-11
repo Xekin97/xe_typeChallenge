a23097890f40dfd8de1c3623256d4d997356e9f3
Xekin Zhuo <xekin2017@gmail.com>
2022-09-11 21:36:31 +0800

    feat: 优化

--- auto-readme.js
+++ auto-readme.js
@@ -3,32 +3,32 @@ const { resolve } = require('path')
 
 const ROOT_PATH = 'https://github.com/Xekin97/xe_typeChallenge/tree/master'
 
+function getFileTitle (filename, diffcult) {
+    if (!filename) return ''
+    return `[${filename.replace('.ts', '')}](${ROOT_PATH}/${diffcult}/${filename})`
+}
+
+
 async function main () {
 
     const ws = fs.createWriteStream(resolve(__dirname, './README.md'))
 
-    let content = "## Xekin's TypeChallenge\n"
+    let content = "## Xekin's TypeChallenge\nEasy|Medium|Hard\n"
 
     console.log('自动写入 readme 开始')
 
-    console.log('读取 easy 文件夹')
-
     const ezFiles = await fs.promises.readdir(resolve(__dirname, './easy'))
-
-    content += "### Easy\n"
-    ezFiles.forEach(filename => {
-        content += `[${filename.replace('.ts', '')}](${ROOT_PATH}/${filename})\n\n`
-    })
-
-    console.log('读取 medium 文件夹')
-
     const mdFiles = await fs.promises.readdir(resolve(__dirname, './medium'))
-    content += "### Medium\n"
-    mdFiles.forEach(filename => {
-        content += `[${filename.replace('.ts', '')}](${ROOT_PATH}/${filename})\n\n`
-    })
-    
+    const hdFiles = await fs.promises.readdir(resolve(__dirname, './hard'))
+
+    let len = Math.max(ezFiles.length, mdFiles.length, hdFiles.length)
+
+    for (let i = 0; i < len; i++) {
+        content += getFileTitle(ezFiles[i], 'easy') + '|' + getFileTitle(mdFiles[i], 'medium') + '|' + getFileTitle(hdFiles[i], 'hard') + '\n'
+    }
     ws.write(content, "utf-8")
+
+    console.log('自动写入 readme 成功')
 
 }
 
